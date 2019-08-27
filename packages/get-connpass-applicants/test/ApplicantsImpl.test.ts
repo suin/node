8378be5acc15c5cb33b57a8274c80a756dbe3c42
suin <suinyeze@gmail.com>
2019-08-27 16:08:11 +0900

    test(get-connpass-applicants): add a test for Applicants.participantsByParticipationType with one pa

--- packages/get-connpass-applicants/test/ApplicantsImpl.test.ts
+++ packages/get-connpass-applicants/test/ApplicantsImpl.test.ts
@@ -2,26 +2,42 @@ import { Applicant } from '../src'
 import { ApplicantsImpl } from '../src/ApplicantsImpl'
 
 describe('Applicants', () => {
-  test('participantsByParticipationType', () => {
-    const participants = [
-      new Applicant('url', 'name1', '参加枠1'),
-      new Applicant('url', 'name2', '参加枠1'),
-      new Applicant('url', 'name3', '参加枠2'),
-      new Applicant('url', 'name4', '参加枠2'),
-    ]
+  describe('participantsByParticipationType', () => {
+    test('one participation type', () => {
+      const participants = [
+        new Applicant('url', 'name1', '参加枠1'),
+        new Applicant('url', 'name2', '参加枠1'),
+      ]
+      const applicants = new ApplicantsImpl(participants, [], [])
+      const types = applicants.participantsByParticipationType
+      expect(Array.from(types.keys())).toEqual(['参加枠1'])
+      expect(types.get('参加枠1')).toMatchObject([
+        new Applicant('url', 'name1', '参加枠1'),
+        new Applicant('url', 'name2', '参加枠1'),
+      ])
+    })
 
-    const applicants = new ApplicantsImpl(participants, [], [])
+    test('two participation types', () => {
+      const participants = [
+        new Applicant('url', 'name1', '参加枠1'),
+        new Applicant('url', 'name2', '参加枠1'),
+        new Applicant('url', 'name3', '参加枠2'),
+        new Applicant('url', 'name4', '参加枠2'),
+      ]
 
-    const types = applicants.participantsByParticipationType
-    expect(Array.from(types.keys())).toEqual(['参加枠1', '参加枠2'])
-    expect(types.get('参加枠1')).toMatchObject([
-      new Applicant('url', 'name1', '参加枠1'),
-      new Applicant('url', 'name2', '参加枠1'),
-    ])
-    expect(types.get('参加枠2')).toMatchObject([
-      new Applicant('url', 'name3', '参加枠2'),
-      new Applicant('url', 'name4', '参加枠2'),
-    ])
-    expect(types.get('存在しない枠')).toBe(undefined)
+      const applicants = new ApplicantsImpl(participants, [], [])
+
+      const types = applicants.participantsByParticipationType
+      expect(Array.from(types.keys())).toEqual(['参加枠1', '参加枠2'])
+      expect(types.get('参加枠1')).toMatchObject([
+        new Applicant('url', 'name1', '参加枠1'),
+        new Applicant('url', 'name2', '参加枠1'),
+      ])
+      expect(types.get('参加枠2')).toMatchObject([
+        new Applicant('url', 'name3', '参加枠2'),
+        new Applicant('url', 'name4', '参加枠2'),
+      ])
+      expect(types.get('存在しない枠')).toBe(undefined)
+    })
   })
 })
