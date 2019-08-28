4d848734202575b32a97dacaddc09b52f8b9ec26
suin <suinyeze@gmail.com>
2019-08-28 16:36:19 +0900

    feat(get-connpass-applicants): remove deprecated `Applicants.prototype.name`
    BREAKING CHANGE: `Applicants.prototype.name` is no longer available. use `Applicants.prototype.displayName` instead

--- packages/get-connpass-applicants/src/Applicant.ts
+++ packages/get-connpass-applicants/src/Applicant.ts
@@ -3,12 +3,11 @@
  */
 export class Applicant {
   readonly nickName: string
-  readonly displayName: string
 
   // noinspection JSUnusedGlobalSymbols
   constructor(
     readonly url: string,
-    name: string,
+    readonly displayName: string,
     readonly participationType: string,
   ) {
     const matches = /^https:\/\/connpass\.com\/user\/([^\/]+)\/$/.exec(url)
@@ -16,14 +15,5 @@ export class Applicant {
       throw new Error(`Invalid URL is given: ${url}`)
     }
     this.nickName = matches[1]
-    this.displayName = name
-  }
-
-  get name(): string {
-    // tslint:disable-next-line:no-console
-    console.warn(
-      'Applicant.prototype.name is deprecated. Use Applicant.prototype.displayName instead.',
-    )
-    return this.displayName
   }
 }
